696a09bfa63cf699a1f0c3229331c44f089598a8
CaptainEureka <captaineureka@gmail.com>
2024-04-21 16:16:35 +0200

    feat(catppuccin): update catppuccin settings to silence deprecation warning

--- boreas/colorschemes/catppuccin.nix
+++ boreas/colorschemes/catppuccin.nix
@@ -1,42 +1,45 @@
 {
   colorschemes.catppuccin = {
     enable = true;
-    background = {
-      dark = "mocha";
-      light = "latte";
-    };
-    disableItalic = true;
-    flavour = "mocha";
-    integrations = {
-      barbar = true;
-      barbecue = {
-        alt_background = true;
-        bold_basename = true;
-        dim_context = true;
-        dim_dirname = true;
+    settings = {
+      background = {
+        dark = "mocha";
+        light = "latte";
       };
-      cmp = true;
-      flash = true;
-      gitsigns = true;
-      harpoon = true;
-      indent_blankline = {
-        enabled = true;
-        colored_indent_levels = true;
+      flavour = "mocha";
+      show_end_of_buffer = true;
+      no_italic = true;
+      transparent_background = false;
+      terminal_colors = true;
+
+      integrations = {
+        barbar = true;
+        barbecue = {
+          alt_background = true;
+          bold_basename = true;
+          dim_context = true;
+          dim_dirname = true;
+        };
+        cmp = true;
+        flash = true;
+        gitsigns = true;
+        harpoon = true;
+        indent_blankline = {
+          enabled = true;
+          colored_indent_levels = true;
+        };
+        lsp_trouble = true;
+        markdown = true;
+        navic.enabled = true;
+        native_lsp.enabled = true;
+        neogit = true;
+        noice = true;
+        notify = true;
+        telescope.enabled = true;
+        treesitter = true;
+        treesitter_context = true;
+        which_key = true;
       };
-      lsp_trouble = true;
-      markdown = true;
-      navic.enabled = true;
-      native_lsp.enabled = true;
-      neogit = true;
-      noice = true;
-      notify = true;
-      telescope.enabled = true;
-      treesitter = true;
-      treesitter_context = true;
-      which_key = true;
     };
-    showBufferEnd = true;
-    terminalColors = true;
-    transparentBackground = false;
   };
 }
