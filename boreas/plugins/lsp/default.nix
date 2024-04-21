dc13b12aa267c4ab8366fb62d30298cfca76ccdb
CaptainEureka <captaineureka@gmail.com>
2024-04-21 16:18:58 +0200

    feat(lsp): enable some new lsp servers

--- boreas/plugins/lsp/default.nix
+++ boreas/plugins/lsp/default.nix
@@ -18,17 +18,24 @@
       };
 
       servers = {
+        docker-compose-language-service.enable = true;
         dockerls.enable = true;
         gopls.enable = true;
         gleam.enable = true;
+        html.enable = true;
+        htmx.enable = true;
         jsonls.enable = true;
+        marksman.enable = true;
         nil_ls.enable = true;
         lua-ls.enable = true;
         pyright.enable = true;
         ruff-lsp.enable = true;
+        taplo.enable = true;
+        tailwindcss.enable = true;
         typst-lsp.enable = true;
         tsserver.enable = true;
         terraformls.enable = true;
+        sqls.enable = true;
         yamlls.enable = true;
       };
     };
