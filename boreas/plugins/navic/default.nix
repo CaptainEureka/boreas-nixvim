92aca38c2ba48de2dd1dea48cfaadcb23f57daf3
CaptainEureka <captaineureka@gmail.com>
2024-09-17 09:57:06 +0200

    feat(navic): update settings syntax

--- boreas/plugins/navic/default.nix
+++ boreas/plugins/navic/default.nix
@@ -1,22 +1,26 @@
 {
   plugins.navic = {
     enable = true;
-    separator = "   ";
-    lsp.autoAttach = true;
-    lsp.preference = [
-      "nil_ls"
-      "dockerls"
-      "gopls"
-      "gleam"
-      "jsonls"
-      "lua-ls"
-      "pyright"
-      "ruff-lsp"
-      "typst-lsp"
-      "tsserver"
-      "terraformls"
-      "yamlls"
-      "efm"
-    ];
+    settings = {
+      separator = "   ";
+      lsp = {
+        auto_attach = true;
+        preference = [
+          "nil_ls"
+          "dockerls"
+          "gopls"
+          "gleam"
+          "jsonls"
+          "lua-ls"
+          "pyright"
+          "ruff-lsp"
+          "typst-lsp"
+          "tsserver"
+          "terraformls"
+          "yamlls"
+          "efm"
+        ];
+      };
+    };
   };
 }
