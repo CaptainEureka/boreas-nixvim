a82bf38d4b7caa640a9d94ca6b125b8ec2fd2e1f
CaptainEureka <captaineureka@gmail.com>
2024-11-03 15:19:38 +0100

    update(noice): update to new syntax

--- boreas/plugins/noice/default.nix
+++ boreas/plugins/noice/default.nix
@@ -3,12 +3,12 @@
     noice = {
       enable = true;
 
-      lsp.override = {
+      settings.lsp.override = {
         "vim.lsp.util.convert_input_to_markdown_lines" = true;
         "vim.lsp.util.stylize_markdown" = true;
         "cmp.entry.get_documentation" = true;
       };
-      presets = {
+      settings.presets = {
         bottom_search = true;
         command_palette = false;
         long_message_to_split = true;
