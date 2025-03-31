18f4c1510208c5fbd86dbf563343670b34de1db8
CaptainEureka <captaineureka@gmail.com>
2025-03-31 16:13:46 +0200

    fix(obsidian): disable frontmatter control from obsidian.nvim

--- boreas/plugins/obsidian/default.nix
+++ boreas/plugins/obsidian/default.nix
@@ -7,6 +7,7 @@
   plugins.obsidian = {
     enable = pkgs.stdenv.isDarwin;
     settings = {
+      disable_frontmatter = true;
       completion.blink = config.plugins.blink-cmp.enable;
       templates.subdir = "templates";
       picker.name = "telescope.nvim";
