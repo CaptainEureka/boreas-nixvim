d1a1979115cf53e697e728297edf9ac7ea0cd1a9
CaptainEureka <captaineureka@gmail.com>
2025-07-04 08:59:54 +0200

    flake.lock: Update

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     systems.url = "github:nix-systems/default";
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/25.05";
     nixvim.url = "github:nix-community/nixvim";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
