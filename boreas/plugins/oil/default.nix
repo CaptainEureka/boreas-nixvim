13bc17bb928740c95b599795ee4c04ce8847115b
CaptainEureka <captaineureka@gmail.com>
2025-08-05 10:06:44 +0200

    feat(oil): Adds keymap

--- boreas/plugins/oil/default.nix
+++ boreas/plugins/oil/default.nix
@@ -1,6 +1,18 @@
 {
+  config,
+  lib,
+  ...
+}: {
   plugins.oil = {
     enable = true;
     settings.use_default_keymaps = true;
   };
+
+  keymaps = lib.mkIf config.plugins.oil.enable [
+    {
+      key = "<leader>e";
+      action = "<CMD>:Oil<CR>";
+      options.desc = "explore";
+    }
+  ];
 }
