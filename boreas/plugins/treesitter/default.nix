e3d67d64b750b710d77f2d978b989ffe6541fb23
CaptainEureka <captaineureka@gmail.com>
2025-10-29 20:38:38 +0100

    fix(treesitter): deprecated settings

--- boreas/plugins/treesitter/default.nix
+++ boreas/plugins/treesitter/default.nix
@@ -11,12 +11,14 @@
 
     treesitter-refactor = {
       enable = true;
-      highlightDefinitions = {
-        enable = true;
-        clearOnCursorMove = true;
+      settings = {
+        highlight_definitions = {
+          enable = true;
+          clear_on_cursor_move = true;
+        };
+        smart_rename.enable = true;
+        navigation.enable = true;
       };
-      smartRename.enable = true;
-      navigation.enable = true;
     };
 
     treesitter-context = {
@@ -26,63 +28,65 @@
 
     treesitter-textobjects = {
       enable = true;
-      lspInterop = {
-        enable = true;
-        border = "single";
-        peekDefinitionCode = {
-          "<leader>df" = {
-            query = "@function.outer";
-            desc = "Peek definition outer function";
+      settings = {
+        lsp_interop = {
+          enable = true;
+          border = "single";
+          peek_definition_code = {
+            "<leader>df" = {
+              query = "@function.outer";
+              desc = "Peek definition outer function";
+            };
+            "<leader>dF" = {
+              query = "@class.outer";
+              desc = "Peek definition outer class";
+            };
           };
-          "<leader>dF" = {
-            query = "@class.outer";
-            desc = "Peek definition outer class";
+        };
+        move = {
+          enable = true;
+          goto_next_start = {
+            "]m" = "@function.outer";
+            "]]" = "@class.outer";
+          };
+          goto_previous_start = {
+            "[m" = "@function.outer";
+            "[[" = "@class.outer";
+          };
+          goto_next_end = {
+            "]M" = "@function.outer";
+            "][" = "@class.outer";
+          };
+          goto_previous_end = {
+            "[M" = "@function.outer";
+            "[]" = "@class.outer";
           };
         };
-      };
-      move = {
-        enable = true;
-        gotoNextStart = {
-          "]m" = "@function.outer";
-          "]]" = "@class.outer";
+        swap = {
+          enable = true;
+          swap_next = {
+            "<leader>a" = "@parameters.inner";
+          };
+          swap_previous = {
+            "<leader>A" = "@parameters.outer";
+          };
         };
-        gotoPreviousStart = {
-          "[m" = "@function.outer";
-          "[[" = "@class.outer";
-        };
-        gotoNextEnd = {
-          "]M" = "@function.outer";
-          "][" = "@class.outer";
-        };
-        gotoPreviousEnd = {
-          "[M" = "@function.outer";
-          "[]" = "@class.outer";
-        };
-      };
-      swap = {
-        enable = true;
-        swapNext = {
-          "<leader>a" = "@parameters.inner";
-        };
-        swapPrevious = {
-          "<leader>A" = "@parameters.outer";
-        };
-      };
-      select = {
-        enable = true;
-        lookahead = true;
-        keymaps = {
-          "aa" = "@parameter.outer";
-          "ia" = "@parameter.inner";
-          "af" = "@function.outer";
-          "if" = "@function.inner";
-          "ac" = "@class.outer";
-          "ic" = "@class.inner";
-          "ii" = "@conditional.inner";
-          "ai" = "@conditional.outer";
-          "il" = "@loop.inner";
-          "al" = "@loop.outer";
-          "at" = "@comment.outer";
+        select = {
+          enable = true;
+          lookahead = true;
+          keymaps = {
+            "aa" = "@parameter.outer";
+            "ia" = "@parameter.inner";
+            "af" = "@function.outer";
+            "if" = "@function.inner";
+            "ac" = "@class.outer";
+            "ic" = "@class.inner";
+            "ii" = "@conditional.inner";
+            "ai" = "@conditional.outer";
+            "il" = "@loop.inner";
+            "al" = "@loop.outer";
+            "at" = "@comment.outer";
+          };
         };
       };
     };
