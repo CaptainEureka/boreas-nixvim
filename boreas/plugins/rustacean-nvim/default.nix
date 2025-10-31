3cd8a9267c04f4c9a8e5e28d8cbb6ae87eb992d8
CaptainEureka <captaineureka@gmail.com>
2025-10-31 14:20:58 +0100

    fix(rust): use default settings

--- boreas/plugins/rustacean-nvim/default.nix
+++ boreas/plugins/rustacean-nvim/default.nix
@@ -2,12 +2,6 @@
   plugins.rustaceanvim = {
     enable = true;
     settings = {
-      server.default_settings.files.excludeDirs = [
-        ".devenv"
-        ".direnv"
-        "target"
-      ];
-
       completion = {autocomplete = true;};
     };
   };
