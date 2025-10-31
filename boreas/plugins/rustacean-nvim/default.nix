d40b5415c08e7dab798350dfa0f8d42fa2d7981a
CaptainEureka <captaineureka@gmail.com>
2025-10-31 14:41:06 +0100

    fix(rust): use rust-analyzer settings

--- boreas/plugins/rustacean-nvim/default.nix
+++ boreas/plugins/rustacean-nvim/default.nix
@@ -2,6 +2,15 @@
   plugins.rustaceanvim = {
     enable = true;
     settings = {
+      server.default_settings.rust-analyzer = {
+        files.excludeDirs = [
+          ".direnv"
+          ".devenv"
+          ".node_modules"
+          ".venv"
+          "result"
+        ];
+      };
       completion = {autocomplete = true;};
     };
   };
